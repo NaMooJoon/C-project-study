21c9a29868f4fb60dc0a57850e9dcc1f3159dda5
mamoo <jjag1015@naver.com>
2022-01-12 19:31:25 +0900

    Wolf&Sheep&Cabbage Game
    BFS를 활용하여 wolf and sheep and cabbage game의 정답을 찾는 프로그램을 구현하였다.
    아쉬운 점은 정답을 비교하는 과정에서 hashing을 활용하지 못했다는 점이다.
    추후에 hashing을 활용해서 좀 더 깔끔하게 프로그램을 짤 수 있으면 된다.

---
+++ wolfSheepPerson/test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+typedef struct {
+    int p;
+    int w;
+    int s;
+    int c;
+} Per;
+
+struct per{
+    int p;
+    int w;
+    int s;
+    int c;
+};
+
+int main(void) {
+    printf("started: 0000\n");
+    Per game;
+    game.p = 0;
+    game.w = 0;
+    game.s = 0;
+    game.c = 0;
+
+    game.p = 1;
+    
+
+    return 0;
+}
